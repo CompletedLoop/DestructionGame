f47676e289a643c1e6967a1bac3e3162b6d61fe8
CompletedLoop <163626738+CompletedLoop@users.noreply.github.com>
2024-06-27 14:16:57 -0400

    .

--- src/shared/Skills/Base/m1.ts
+++ src/shared/Skills/Base/m1.ts
@@ -1,9 +1,21 @@
-import { Skill, SkillDecorator } from "@rbxts/wcs";
+import { Message, Skill, SkillDecorator } from "@rbxts/wcs";
 
 @SkillDecorator
 export class m1 extends Skill {
 	public OnStartServer(): void {
-		print("m1")
-		this.ApplyCooldown(.5)
+		print("attack initiated on server")
+		
+		this.serverSaidSomething(`Hello Client`)
+
+		this.ApplyCooldown(1)
+	}
+
+	public OnStartClient(): void {
+		print("attack initiated on client")
+	}
+
+	@Message({ Type: "Event", Destination: "Client", })
+	protected serverSaidSomething(thing: string) {
+		print(thing)
 	}
 }
