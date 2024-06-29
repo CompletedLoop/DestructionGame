51f9bc3d5e388f92a5ae24b8d4435443849c781a
perce <completedloop@gmail.com>
2024-06-28 23:05:00 -0400

    Testing out VoxelDestruct

--- src/server/services/BoomToolServer.ts
+++ src/server/services/BoomToolServer.ts
@@ -8,13 +8,15 @@ import VoxelModule from "shared/Luau/VoxelModule";
 export class BoomToolServer implements OnStart {
 	onStart() {
 		Events.BoomTool.connect((player: Player, cframe: CFrame, radius: number , power: number) => {
-			// let voxels = VoxelModule.DestroyInRadius(radius, cframe)
-			// voxels.forEach((voxel: Part) => {
-			// 	voxel.Anchored = false
-			// 	voxel.AssemblyAngularVelocity = new Random().NextUnitVector().mul(80)
-			// })
-			let voxels = DestructionModule.VoxelizeInRadius(radius, cframe, 3)
-			DestructionModule.PassVoxelsToClients(voxels, cframe, power)
+			let voxels = VoxelModule.DestroyInRadius(radius, cframe)
+			voxels.forEach((voxel: Part) => {
+				voxel.Anchored = false
+				voxel.Size = voxel.Size.mul(.95)
+				voxel.AssemblyAngularVelocity = new Random().NextUnitVector().mul(80)
+			})
+
+			// let voxels = DestructionModule.VoxelizeInRadius(radius, cframe, 3)
+			// DestructionModule.PassVoxelsToClients(voxels, cframe, power)
 		})
 	}
 }
--- src/shared/Luau/VoxelModule.lua
+++ src/shared/Luau/VoxelModule.lua
@@ -4,7 +4,26 @@ local VoxelDestruct = require(ReplicatedStorage.Modules.VoxelDestruction)
 local VoxelModule = {}
 
 VoxelModule.DestroyInRadius = function(radius, cframe)
-	local voxels, walls = VoxelDestruct.destroy(cframe, Vector3.one * radius)
+	local voxels, walls = VoxelDestruct.destroy(
+		cframe, Vector3.one * radius,
+		Enum.PartType.Ball,
+		OverlapParams.new(),
+		2,
+		nil,
+		nil,
+		nil,
+		true,
+		10
+	)
+
+	-- if true then
+	-- 	for _, voxel: Part in voxels do
+	-- 		voxel.BrickColor = BrickColor.random()
+	-- 	end
+	-- 	for _, voxel: Part in walls do
+	-- 		voxel.BrickColor = BrickColor.random()
+	-- 	end
+	-- end
 	return voxels
 end
 
